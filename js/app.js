088354752f89b56e72c48d4e6fd17e2541ce0edb
Sup-Dev <sourabh.dev90@gmail.com>
2016-05-10 19:15:09 +0530

    feat: inmplemented collision

--- js/app.js
+++ js/app.js
@@ -17,7 +17,18 @@ var Enemy = function(pos, speed) {
     // start position
     this.x = 0;
     this.y = (this.ydist)*(1 + pos) - ((this.ydist)/2) + 20;
+    
+    // bug collider x-points
+    this.x1collider = this.x;
+    this.x2collider = this.x + 100;    
 };
+
+// update collsion points
+Enemy.prototype.updateColliders = function() {
+    // bug collider x-points
+    this.x1collider = this.x - 50;
+    this.x2collider = this.x + 100;
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -30,12 +41,23 @@ Enemy.prototype.update = function(dt) {
         this.x = -100;
     } else {
         this.x += this.speed * dt;
+        this.updateColliders();
     }
+    
+    // test collision with player
+    if ( Math.abs(this.y - player.y) == 20)
+    if (((this.x1collider < player.x1collider) && (this.x2collider > player.x1collider)) ||
+       ((this.x1collider < player.x2collider) && (this.x2collider > player.x2collider))) {
+        // reset player position
+        player.reset();
+        console.log('collision!');
+        console.log(this.y, player.y);
+    }    
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);    
 };
 
 // Now write your own player class
@@ -56,7 +78,24 @@ var Player = function() {
     // player speed
     this.xspeed = 0;
     this.yspeed = 0;
+    
+    // player collider x-points
+    this.x1collider = this.x + this.xdist - (this.xdist)/2 -35;
+    this.x2collider = this.x1collide + 70;    
 };
+
+// update collsion points
+Player.prototype.updateColliders = function() {
+    // bug collider x-points
+    this.x1collider = this.x + this.xdist - (this.xdist)/2 -35;
+    this.x2collider = this.x1collide + 70;
+}
+
+Player.prototype.reset = function() {
+    // player start position
+    this.x = (this.xdist)*2;
+    this.y = (this.ydist)*5 - ((this.ydist)/2);
+}
 
 Player.prototype.update = function(dt) {
     //console.log(this.x, this.y);
@@ -70,6 +109,7 @@ Player.prototype.update = function(dt) {
         this.x = 404;
     } else {
         this.x += (this.xspeed * this.xdist);
+        this.updateColliders();
     }
 
     // check bounds y-axis
@@ -79,6 +119,7 @@ Player.prototype.update = function(dt) {
         this.y = 41.5;
     } else {
         this.y += (this.yspeed * this.ydist);
+        this.updateColliders();
     }
 
     // reset speed
@@ -88,7 +129,7 @@ Player.prototype.update = function(dt) {
 
 Player.prototype.render = function() {
   //console.log(this.x, this.y);
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);    
 };
 
 Player.prototype.handleInput = function(keyPress) {
